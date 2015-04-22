92ed9dac040caf15bb0e7c589e915a681b3ea6f2
Chiel Kunkels <chiel@kunkels.me>
2015-04-23 00:52:14 +0200

    loadModule -> registerModule

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var match, dir = __dirname + '/node_modules';
 fs.readdirSync(dir).forEach(function(moduleName){
 	match = moduleName.match(/^omni\.cm-(.+)/);
 	if (!match) return;
-	omni.loadModule(dir + '/' + moduleName);
+	omni.registerModule(dir + '/' + moduleName);
 });
 
 dir = __dirname + '/modules';
 if (fs.existsSync(dir)){
 	fs.readdirSync(dir).forEach(function(moduleName){
-		omni.loadModule(dir + '/' + moduleName);
+		omni.registerModule(dir + '/' + moduleName);
 	});
 }
 
